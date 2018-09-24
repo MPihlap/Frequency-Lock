5ed2da1ecfd4eed9246b64f99bbcec02a09e7e31
Meelis Pihlap <meelis.pihlap@estcube.eu>
2018-09-24 17:21:12 +0300

    UART test, not working yet

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
-#define USE_STDPERIPH_DRIVER
+//#define USE_STDPERIPH_DRIVER
 #include "stm32f4xx.h"
-//#include "stm32f4xx_usart.h"
+#include "stm32f4xx_usart.h"
  
 
 //Quick hack, approximately 1ms delay
@@ -19,15 +19,20 @@ USART_InitTypeDef uartInit;
 USART_ClockInitTypeDef uartClock;
 
 //Flash orange LED at about 1hz
-int main(void)
-{
+int main(void) {
+
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+    //GPIO_PinAFConfig(GPIO_PIN_ ); // Not sure if this is necessary or not...
     USART_StructInit(&uartInit);
     USART_Init(USART1, &uartInit);
     uartInit.USART_BaudRate = 115200;
 
     USART_ClockStructInit(&uartClock);
-    USART_ClockInit(USART1, &uartClock);
+    USART_ClockInit(USART2, &uartClock);
     USART_Cmd(USART1, ENABLE);
+
+    uint16_t dataOut = 0xA55A;
 
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;  // enable the clock to GPIOD
     __asm("dsb");                         // stall instruction pipeline, until instruction completes, as
@@ -37,5 +42,8 @@ int main(void)
     for (;;) {
        ms_delay(500);
        GPIOD->ODR ^= (1 << 13);           // Toggle the pin 
+       
+       USART_SendData(USART2, dataOut);
+       USART_SendBreak(USART2);
     }
 }
