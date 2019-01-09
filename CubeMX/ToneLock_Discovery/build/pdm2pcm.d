dc73adaa1b1046ffe0e9d82be6da7f6890b95fb1
Meelis Pihlap <meelis.pihlap@estcube.eu>
2019-01-09 10:11:47 +0200

    Got data from mic

---
+++ CubeMX/ToneLock_Discovery/build/pdm2pcm.d
@@ -0,0 +1,6 @@
+build/pdm2pcm.o: Src/pdm2pcm.c Inc/pdm2pcm.h \
+ Middlewares/ST/STM32_Audio/Addons/PDM/Inc/pdm2pcm_glo.h
+
+Inc/pdm2pcm.h:
+
+Middlewares/ST/STM32_Audio/Addons/PDM/Inc/pdm2pcm_glo.h:
