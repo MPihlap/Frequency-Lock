7e416d390fa8e30fcc56a61afa4815d9b08a0b1f
Meelis Pihlap <meelispihlap@estcube.eu>
2019-03-09 13:18:48 +0200

    Refactored project structure and removed broken code

--- firmware/src/stm32f4xx_it.c
+++ firmware/src/stm32f4xx_it.c
@@ -1,175 +1,219 @@
+/**
+  ******************************************************************************
+  * @file    stm32f4xx_it.c
+  * @brief   Interrupt Service Routines.
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2019 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx.h"
 #include "stm32f4xx_it.h"
-#include "stm32f4xx_conf.h"
-#include "main.h"
-#include "pdm2pcm_glo.h"
 
-uint32_t InternalBufferSize = 0;
-uint32_t Data_Status = 0;
-int x = 0;
-int y = 0;
+/* USER CODE BEGIN 0 */
+uint32_t local_pdm_pointer;
+/* USER CODE END 0 */
+
+/* External variables --------------------------------------------------------*/
+extern I2S_HandleTypeDef hi2s2;
+
+/******************************************************************************/
+/*            Cortex-M4 Processor Interruption and Exception Handlers         */ 
+/******************************************************************************/
 
 /**
-  * @brief   This function handles NMI exception.
-  * @param  None
-  * @retval None
-  */
-__attribute__((weak)) void NMI_Handler(void)
+* @brief This function handles Non maskable interrupt.
+*/
+void NMI_Handler(void)
 {
+  /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
+
+  /* USER CODE END NonMaskableInt_IRQn 0 */
+  /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
+
+  /* USER CODE END NonMaskableInt_IRQn 1 */
 }
 
 /**
-  * @brief  This function handles Hard Fault exception.
-  * @param  None
-  * @retval None
-  */
-__attribute__((weak)) void HardFault_Handler(void)
+* @brief This function handles Hard fault interrupt.
+*/
+void HardFault_Handler(void)
 {
-  /* Go to infinite loop when Hard Fault exception occurs */
+  /* USER CODE BEGIN HardFault_IRQn 0 */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+  /* USER CODE END HardFault_IRQn 0 */
   while (1)
   {
+    /* USER CODE BEGIN W1_HardFault_IRQn 0 */
+    
+    /* USER CODE END W1_HardFault_IRQn 0 */
   }
+  /* USER CODE BEGIN HardFault_IRQn 1 */
+
+  /* USER CODE END HardFault_IRQn 1 */
 }
 
 /**
-  * @brief  This function handles Memory Manage exception.
-  * @param  None
-  * @retval None
-  */
-__attribute__((weak)) void MemManage_Handler(void)
+* @brief This function handles Memory management fault.
+*/
+void MemManage_Handler(void)
 {
-  /* Go to infinite loop when Memory Manage exception occurs */
+  /* USER CODE BEGIN MemoryManagement_IRQn 0 */
+
+  /* USER CODE END MemoryManagement_IRQn 0 */
   while (1)
   {
+    /* USER CODE BEGIN W1_MemoryManagement_IRQn 0 */
+    /* USER CODE END W1_MemoryManagement_IRQn 0 */
   }
+  /* USER CODE BEGIN MemoryManagement_IRQn 1 */
+
+  /* USER CODE END MemoryManagement_IRQn 1 */
 }
 
 /**
-  * @brief  This function handles Bus Fault exception.
-  * @param  None
-  * @retval None
-  */
-__attribute__((weak)) void BusFault_Handler(void)
+* @brief This function handles Pre-fetch fault, memory access fault.
+*/
+void BusFault_Handler(void)
 {
-  /* Go to infinite loop when Bus Fault exception occurs */
+  /* USER CODE BEGIN BusFault_IRQn 0 */
+
+  /* USER CODE END BusFault_IRQn 0 */
   while (1)
   {
+    /* USER CODE BEGIN W1_BusFault_IRQn 0 */
+    /* USER CODE END W1_BusFault_IRQn 0 */
   }
+  /* USER CODE BEGIN BusFault_IRQn 1 */
+
+  /* USER CODE END BusFault_IRQn 1 */
 }
 
 /**
-  * @brief  This function handles Usage Fault exception.
-  * @param  None
-  * @retval None
-  */
-__attribute__((weak)) void UsageFault_Handler(void)
+* @brief This function handles Undefined instruction or illegal state.
+*/
+void UsageFault_Handler(void)
 {
-  /* Go to infinite loop when Usage Fault exception occurs */
+  /* USER CODE BEGIN UsageFault_IRQn 0 */
+
+  /* USER CODE END UsageFault_IRQn 0 */
   while (1)
   {
+    /* USER CODE BEGIN W1_UsageFault_IRQn 0 */
+    /* USER CODE END W1_UsageFault_IRQn 0 */
   }
+  /* USER CODE BEGIN UsageFault_IRQn 1 */
+
+  /* USER CODE END UsageFault_IRQn 1 */
 }
 
 /**
-  * @brief  This function handles SVCall exception.
-  * @param  None
-  * @retval None
-  */
-__attribute__((weak)) void SVC_Handler(void)
+* @brief This function handles System service call via SWI instruction.
+*/
+void SVC_Handler(void)
 {
+  /* USER CODE BEGIN SVCall_IRQn 0 */
+
+  /* USER CODE END SVCall_IRQn 0 */
+  /* USER CODE BEGIN SVCall_IRQn 1 */
+
+  /* USER CODE END SVCall_IRQn 1 */
 }
 
 /**
-  * @brief  This function handles Debug Monitor exception.
-  * @param  None
-  * @retval None
-  */
-__attribute__((weak)) void DebugMon_Handler(void)
+* @brief This function handles Debug monitor.
+*/
+void DebugMon_Handler(void)
 {
+  /* USER CODE BEGIN DebugMonitor_IRQn 0 */
+
+  /* USER CODE END DebugMonitor_IRQn 0 */
+  /* USER CODE BEGIN DebugMonitor_IRQn 1 */
+
+  /* USER CODE END DebugMonitor_IRQn 1 */
 }
 
 /**
-  * @brief  This function handles PendSVC exception.
-  * @param  None
-  * @retval None
-  */
-__attribute__((weak)) void PendSV_Handler(void)
+* @brief This function handles Pendable request for system service.
+*/
+void PendSV_Handler(void)
 {
+  /* USER CODE BEGIN PendSV_IRQn 0 */
+
+  /* USER CODE END PendSV_IRQn 0 */
+  /* USER CODE BEGIN PendSV_IRQn 1 */
+
+  /* USER CODE END PendSV_IRQn 1 */
 }
 
 /**
-  * @brief  This function handles SysTick Handler.
-  * @param  None
-  * @retval None
-  */
-__attribute__((weak)) void SysTick_Handler(void)
+* @brief This function handles System tick timer.
+*/
+void SysTick_Handler(void)
 {
+  /* USER CODE BEGIN SysTick_IRQn 0 */
 
+  /* USER CODE END SysTick_IRQn 0 */
+  HAL_IncTick();
+  HAL_SYSTICK_IRQHandler();
+  /* USER CODE BEGIN SysTick_IRQn 1 */
+
+  /* USER CODE END SysTick_IRQn 1 */
 }
 
+/******************************************************************************/
+/* STM32F4xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32f4xx.s).                    */
+/******************************************************************************/
 
-#define MAX_WORDLEN 10
-volatile char received_str[MAX_WORDLEN + 1];
-extern void USART_puts(USART_TypeDef *USARTx, volatile char *str);
-
-// USART2 Interrupt request handler for ALL (Tx/Rx) USART2 interrupts
-void USART2_IRQHandler(void){
-
-  // Check the Interrupt status to ensure the Rx interrupt was triggered, not Tx
-  if( USART_GetITStatus(USART2, USART_IT_RXNE)){
-    static int cnt = 0;
-    // Get the byte that was transferred
-    char ch = USART2->DR;
-
-    // Check for "Enter" key, or Maximum characters
-    if((ch != '\n') && (cnt < MAX_WORDLEN)){
-      received_str[cnt++] = ch;
-    }
-    else{
-      received_str[cnt] = '\0';
-      cnt = 0;
-      USART_puts(USART2, received_str);
-    }
-  }
-
+/**
+* @brief This function handles SPI2 global interrupt.
+*/
+void SPI2_IRQHandler(void)
+{
+  /* USER CODE BEGIN SPI2_IRQn 0 */
+  
+  // extern uint16_t dataBuffer[];
+  // if (HAL_SPI_GetState(&hi2s2) == HAL_SPI_STATE_READY){
+  //   HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
+  //   HAL_Delay(200);
+  //   // HAL_I2S_Receive_IT(&hi2s2, dataBuffer, 1024);
+  // }
+  /* USER CODE END SPI2_IRQn 0 */
+  HAL_I2S_IRQHandler(&hi2s2);
+  /* USER CODE BEGIN SPI2_IRQn 1 */
+  /* USER CODE END SPI2_IRQn 1 */
 }
 
+/* USER CODE BEGIN 1 */
 
-
-void SPI2_IRQHandler(void){
-  extern PDM_Filter_Handler_t Filter;
-  extern uint16_t* AudioRecBuf;
-  extern uint16_t* WriteBuf;
-  extern uint16_t  PDM_Input_Buffer[];
-  extern uint16_t PCM_Output_Buffer[];
-
-  u16 volume;
-  u16 app;
-
-  // Check if new data are available in SPI data register
-  if (SPI_GetITStatus(SPI2, SPI_I2S_IT_RXNE) != RESET){
-    // Read received data and save it in internal table
-    app = SPI_I2S_ReceiveData(SPI2);
-    PDM_Input_Buffer[InternalBufferSize++] = (uint8_t)app;
-    //PDM_Input_Buffer[InternalBufferSize++] = /*(uint8_t)*/HTONS(app);
-
-    // Check to prevent overflow condition
-    if (InternalBufferSize >= PDM_Input_Buffer_SIZE){
-      InternalBufferSize = 0;
-
-      volume = 50;
-
-      PDM_Filter((uint8_t *)PDM_Input_Buffer, (uint16_t *)AudioRecBuf/*PCM_Output_Buffer*/, &Filter);
-
-      Data_Status = 1;
-    }
-  }
-}
-
-
-
-
-
-
-
-
-
+/* USER CODE END 1 */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
