652dfda3ad508897f3b3619a7ec2522c06c46c61
Meelis Pihlap <meelis.pihlap@estcube.eu>
2019-03-15 10:21:37 +0200

    Fixed names of src and inc folders

---
+++ firmware/Inc/note_freq.h
@@ -0,0 +1,19 @@
+// Different pitches in Hz
+
+#define G5 787
+#define A5 884
+#define B5 992
+#define C6 1051
+#define D6 1180
+#define E6 1324
+#define F6_SHARP 1486
+#define G6 1575
+
+// How many notes need to be played
+#define LOCK_STAGES MY_HEART_WILL_GO_ON_LEN
+#define THE_LICK \
+  { G5, A5, B5, C6, B5, G5, A5 }
+#define THE_LICK_LEN 7
+#define MY_HEART_WILL_GO_ON \
+  { G5, A5, B5, A5, G5, A5, D6, C6, B5, G5 }
+#define MY_HEART_WILL_GO_ON_LEN 10
